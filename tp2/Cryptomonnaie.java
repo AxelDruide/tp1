d3ab0a59f6a338cc87f9d6b908151d9f944dc0e3
Letellier Axel <axelletellierr@gmail.com>
2025-04-21 16:20:56 +0200

    Ajout des fichiers de tp1

---
+++ tp2/Cryptomonnaie.java
@@ -0,0 +1,9 @@
+public class Cryptomonnaie {
+    private String nom;
+    private double valeurDeJeton;
+
+    public Cryptomonnaie(String nom, double valeurDeJeton) {
+        this.nom = nom;
+        this.valeurDeJeton = valeurDeJeton;
+    }
+}
